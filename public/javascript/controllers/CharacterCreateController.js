10020ccaa350a20ca62db9c0d47dbaa37fbeb30e
Jeremy Doucet <jeremy.doucet93@gmail.com>
2015-12-01 11:47:34 -0800

    character create html page done

--- public/javascript/controllers/CharacterCreateController.js
+++ public/javascript/controllers/CharacterCreateController.js
@@ -3,7 +3,13 @@
   angular.module('app').controller('CharacterCreateController', CharacterCreateController);
   function CharacterCreateController($state, $stateParams, CharacterFactory) {
       var vm = this;
-
       if(!$stateParams.factionId) $state.go('Home');
+      vm.character = { isJedi: true, faction: $stateParams.factionId };
+
+      vm.createCharacter = function() {
+        CharacterFactory.createCharacter(vm.character).then(function(res) {
+          $state.go('FactionDetails', { id: $stateParams.factionId });
+        });
+      };
   }
 })();
